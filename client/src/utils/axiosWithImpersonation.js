60246c7fd2a43b44d6f89974bcfcde560d6fb80f
Juno Lee <0312juno@gmail.com>
2025-07-02 21:40:17 -0700

    content dashboard fix

--- client/src/utils/axiosWithImpersonation.js
+++ client/src/utils/axiosWithImpersonation.js
@@ -1,19 +1,24 @@
 import axios from 'axios';
 import { useImpersonation } from '../context/ImpersonationContext';
+import { useMemo } from 'react';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
 
 // Custom hook to get an axios instance with impersonation header
 export const useAxiosWithImpersonation = () => {
   const { impersonatedUserId } = useImpersonation();
-  const instance = axios.create({ baseURL: API_BASE_URL });
 
-  instance.interceptors.request.use((config) => {
-    if (impersonatedUserId) {
-      config.headers['x-impersonate-user-id'] = impersonatedUserId;
-    }
-    return config;
-  });
+  // Memoize the axios instance so it is not recreated on every render
+  const instance = useMemo(() => {
+    const axiosInstance = axios.create({ baseURL: API_BASE_URL });
+    axiosInstance.interceptors.request.use((config) => {
+      if (impersonatedUserId) {
+        config.headers['x-impersonate-user-id'] = impersonatedUserId;
+      }
+      return config;
+    });
+    return axiosInstance;
+  }, [impersonatedUserId]);
 
   return instance;
 }; 
