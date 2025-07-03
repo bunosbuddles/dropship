906f5cde3d8c8696229f979e26c3bdf1766444d6
Juno Lee <0312juno@gmail.com>
2025-07-02 22:11:10 -0700

    superuser token implementation

--- client/src/utils/axiosWithImpersonation.js
+++ client/src/utils/axiosWithImpersonation.js
@@ -11,8 +11,16 @@ export const useAxiosWithImpersonation = () => {
   const { impersonatedUserId } = useImpersonation();
 
   useEffect(() => {
-    // Add a request interceptor that always uses the latest impersonatedUserId
+    // Add a request interceptor that always uses the latest impersonatedUserId and JWT token
     const interceptor = axiosInstance.interceptors.request.use((config) => {
+      // Always set the JWT token from localStorage
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers['x-auth-token'] = token;
+      } else {
+        delete config.headers['x-auth-token'];
+      }
+      // Set or remove impersonation header
       if (impersonatedUserId) {
         config.headers['x-impersonate-user-id'] = impersonatedUserId;
       } else {
